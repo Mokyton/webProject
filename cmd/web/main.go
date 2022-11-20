bdd35f613988ecc44761f720a66ef778ec7a6fab
Mokyton <masudasrorov561@gmail.com>
2022-11-20 15:08:26 +0300

    feature:/create pool of connections to mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"github.com/Mokyton/webProject/pkg/models/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -11,18 +14,28 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Название MySQl источника данных")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	mux := http.NewServeMux()
@@ -41,7 +54,7 @@ func main() {
 	}
 
 	infoLog.Printf("Запуск сервера на %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
@@ -67,4 +80,15 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 		return nil, err
 	}
 	return f, nil
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
